0301b6968325c22345fcae38d01169d941ddd44f
Nemanja Milićević <the.nemanja.milicevic@gmail.com>
2018-06-17 21:28:38 +0200

    Show training progress bar

--- kvmemnn/train.py
+++ kvmemnn/train.py
@@ -1,10 +1,10 @@
 import argparse
-from time import time
 
 import numpy as np
 import torch
 from torch import optim
 from torch.nn import CosineEmbeddingLoss
+from tqdm import tqdm
 
 from dataset import Dataset
 from memory import KeyValueMemory
@@ -12,12 +12,11 @@ from module import KVMemoryNN
 
 
 BATCH_SIZE = 64
-EPOCHS = 100
 EMBEDDING_DIM = 256
+EPOCHS = 100
 
 
 def train(device):
-    print('-- Loading dataset\n')
     dataset = Dataset(batch_size=BATCH_SIZE)
     kv_memory = KeyValueMemory(dataset)
 
@@ -26,57 +25,57 @@ def train(device):
     model.train()
 
     criterion = CosineEmbeddingLoss(margin=0.1, size_average=False).to(device=device)
+
     optimizer = optim.Adam(model.parameters())
 
     for epoch in range(EPOCHS):
-        print('Epoch: {}/{}'.format(epoch, EPOCHS))
         losses = []
 
-        for iteration, batch in enumerate(dataset):
-            iter_start = time()
-            optimizer.zero_grad()
+        with tqdm(dataset.iterator,
+                  unit=' batches',
+                  desc='Epoch {:3}/{}'.format(epoch + 1, EPOCHS)) as pb:
+            for batch in pb:
+                optimizer.zero_grad()
 
-            keys_tensor, values_tensor = kv_memory.get(batch.query, batch.response)
+                keys_tensor, values_tensor = kv_memory.batch_address(batch.query,
+                                                                     batch.response,
+                                                                     train=True)
 
-            xe, ye = model(batch.query.to(device=device),
-                           keys_tensor.to(device=device),
-                           values_tensor.to(device=device))
+                xe, ye = model(batch.query.to(device=device),
+                               keys_tensor.to(device=device),
+                               values_tensor.to(device=device))
 
-            targets = -torch.ones(xe.shape[:2], device=device)
-            targets[:, 0] = 1
+                targets = -torch.ones(xe.shape[:2], device=device)
+                targets[:, 0] = 1
 
-            cos_losses = torch.stack([
-                criterion(xe[i, :, :], ye[i, :, :], targets[i, :])
-                for i in range(len(xe))
-            ])
+                cos_losses = torch.stack([
+                    criterion(xe[i, :, :], ye[i, :, :], targets[i, :])
+                    for i in range(len(xe))
+                ])
 
-            loss = torch.sum(cos_losses) / BATCH_SIZE
-            loss.backward()
-            optimizer.step()
+                loss = torch.sum(cos_losses) / len(xe)
+                loss.backward()
+                optimizer.step()
 
-            losses.append(loss.item())
-            iter_end = time()
-            avg_loss = np.mean(losses)
+                losses.append(loss.item())
+                pb.set_postfix_str('Loss: {:.3f}'.format(np.mean(losses)))
 
-            iters_time = time() - iter_start
-            print('Batch: {}/{}\tLoss: {:.3f}\tTime: {:.3f}'.format(iteration,
-                                                                    len(dataset.data.examples) // BATCH_SIZE,
-                                                                    avg_loss,
-                                                                    iters_time))
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--cuda', action='store_true', help='Enable CUDA training')
+    parser = argparse.ArgumentParser(description='Train Key-Value Memory Network on ALICE bot data')
+    parser.add_argument('--cpu', action='store_true', help='Disable CUDA training and train on CPU')
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = parse_args()
 
-    if args.cuda and torch.cuda.is_available():
+    if torch.cuda.is_available() and not args.cpu:
         device = torch.device('cuda')
-        print('\n-- Using CUDA')
+        print('\nUsing CUDA for training')
+        print('Pass \'--cpu\' to disable CUDA and train on CPU')
     else:
         device = torch.device('cpu')
-        print('\n-- Using CPU')
+        print('\nUsing CPU for training')
 
     train(device=device)
