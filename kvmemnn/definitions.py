c30e3dd73430ca737e7fbb07388841abbb20b465
Nemanja Milićević <the.nemanja.milicevic@gmail.com>
2018-06-29 15:28:58 +0200

    Upload new raw data

--- kvmemnn/definitions.py
+++ kvmemnn/definitions.py
@@ -3,6 +3,6 @@ import os
 current_filepath = os.path.dirname(os.path.abspath(__file__))
 
 ROOT_PATH = os.path.abspath(os.path.join(current_filepath, os.pardir))
-DATA_PATH = os.path.join(ROOT_PATH, 'data/raw/tokenized.csv')
+DATA_PATH = os.path.join(ROOT_PATH, 'data/raw/data.csv')
 MEMORY_CACHE_PATH = os.path.join(ROOT_PATH, '.cache/memories')
 MODELS_DIR = os.path.join(ROOT_PATH, 'models/')
